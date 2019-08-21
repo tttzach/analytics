885be4bf21dda3a6d2e612748a17147b9fcb0edd
tttzach <tttzach@gmail.com>
2019-08-21 01:07:13 -0400

    Added hosts and static, fixed graph display

--- analytics/analyticstool.py
+++ analytics/analyticstool.py
@@ -11,7 +11,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-resultPath = os.path.join(BASE_DIR, 'analytics','result.png')
+resultPath = os.path.join(BASE_DIR, 'static/images','result.png')
 
 # In[2]:
 
--- analytics_project/settings.py
+++ analytics_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'homy_-hf0i@&+i(q85uapnxwnc@d3w*x9np_4w3ytze7!8nh*i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'tttzach.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
